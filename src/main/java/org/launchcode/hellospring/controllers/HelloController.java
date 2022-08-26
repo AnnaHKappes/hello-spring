09a0387ddcf6229f7357d2a40e473277787220b4
Anna Kappes <julie_jia678@hotmail.com>
2022-08-25 20:23:39 -0400

    Finished static-responses

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,27 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+//    @GetMapping
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring!";
+//    }
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello, Spring!";
+    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye(){
+        return "Goodbye, Spring!";
+    }
+}
